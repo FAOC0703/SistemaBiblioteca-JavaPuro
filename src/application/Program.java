52dd24d6bcaf5fcf89fa8a37123f216bac5efc34
FAOC0703 <fo7499490@gmail.com>
2026-09-02 15:47:40 -0300

    chore: inicializado o menu de opcoes

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,7 +13,25 @@ public class Program {
 		List<String> autor = new ArrayList<>();
 		List<Integer> ano = new ArrayList<>();
 		List<Integer> quantidade = new ArrayList<>();
+		int option = -1;
 		
+		do {
+			System.out.println("==============================");
+			
+			System.out.println("       SISTEMA BIBLIOTECA");
+			System.out.println("==============================");
+			System.out.println();
+			System.out.println("1 - Cadastrar livro");
+			System.out.println("2 - Listar livros");
+			System.out.println("3 - Buscar livro");
+			System.out.println("4 - Emprestar livro");
+			System.out.println("5 - Devolver livro");
+			System.out.println("6 - Remover livro");
+			System.out.println("0 - Sair");
+			System.out.println();
+			System.out.print("Escolha uma opção: ");
+			option = sc.nextInt();
+		} while(option != 0);
 		
 		
 		sc.close();
